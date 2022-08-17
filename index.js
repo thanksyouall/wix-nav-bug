5fa763e35e01a4fa86849753e5bbf8d33efe88a4
thanksyouall <tech.development.mail@gmail.com>
2022-08-17 20:59:24 +0300

    Add nav

--- index.js
+++ index.js
@@ -1,9 +1,129 @@
-/**
- * @format
- */
+import React from 'react';
+import {View, Text, Button, StyleSheet} from 'react-native';
+import {Navigation} from 'react-native-navigation';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+const HomeScreen = props => {
+  return (
+    <View style={styles.root}>
+      <Text>Hello React Native Navigation 👋</Text>
 
-AppRegistry.registerComponent(appName, () => App);
+      <Button
+        title="Push Settings Screen"
+        color="#710ce3"
+        onPress={() =>
+          Navigation.push(props.componentId, {
+            component: {
+              name: 'Settings',
+            },
+          })
+        }
+      />
+    </View>
+  );
+};
+HomeScreen.options = {
+  topBar: {
+    title: {
+      text: 'Home',
+    },
+  },
+  bottomTab: {
+    text: 'Home',
+  },
+};
+
+const SettingsScreen = () => {
+  return (
+    <View style={styles.root}>
+      <Text>Settings Screen</Text>
+    </View>
+  );
+};
+SettingsScreen.options = {
+  topBar: {
+    title: {
+      text: 'Settings',
+    },
+  },
+  bottomTab: {
+    text: 'Settings',
+  },
+};
+
+Navigation.registerComponent('Home', () => HomeScreen);
+Navigation.registerComponent('Settings', () => SettingsScreen);
+
+const tabletAnimations = {
+  push: {
+    content: {
+      translationX: {
+        from: require('react-native').Dimensions.get('window').width,
+        to: 0,
+        duration: 300,
+      },
+    },
+  },
+  pop: {
+    content: {
+      translationX: {
+        from: 0,
+        to: -require('react-native').Dimensions.get('window').width,
+        duration: 300,
+      },
+    },
+  },
+};
+
+Navigation.setDefaultOptions({
+  animations: tabletAnimations,
+  topBar: {
+    drawBehind: true,
+    // visible: false,
+  },
+  bottomTabs: {
+    drawBehind: true,
+    // visible: false,
+  },
+});
+
+Navigation.events().registerAppLaunchedListener(async () => {
+  Navigation.setRoot({
+    root: {
+      bottomTabs: {
+        children: [
+          {
+            stack: {
+              children: [
+                {
+                  component: {
+                    name: 'Home',
+                  },
+                },
+              ],
+            },
+          },
+          {
+            stack: {
+              children: [
+                {
+                  component: {
+                    name: 'Settings',
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  });
+});
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'whitesmoke',
+  },
+});
